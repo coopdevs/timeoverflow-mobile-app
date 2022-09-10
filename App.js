3dc736d3f8787142f1db4ce92ac06fa1c6f9e74a
Pau Perez <saulopefa@gmail.com>
2022-09-10 18:52:01 +0200

    Load webview

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
+import React, { useState, useEffect, useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
+import { WebView } from 'react-native-webview';
 import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+
+const baseUrl = () => {
+  const { releaseChannel } = Constants.manifest;
+  console.log("Constants.manifest = ");
+  console.log(Constants.manifest);
+  console.log("release channel = " + releaseChannel);
+
+  // return (releaseChannel === 'staging') ?
+    // 'https://staging.timeoverflow.org' :
+    // 'https://www.timeoverflow.org';
+  return 'https://staging.timeoverflow.org';
+}
 
 export default function App() {
+  const [currentUrl, setCurrentUrl] = useState(baseUrl());
+
+  const webViewRef = useRef(null);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <WebView
+        ref={ref => webViewRef.current = ref}
+        style={styles.container}
+        source={{ uri: currentUrl }}
+        scalesPageToFit={false}
+        // onNavigationStateChange={handleStateChange}
+      />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
