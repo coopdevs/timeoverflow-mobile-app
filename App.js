9ceeb0a37d77d6ea025d2ee5bf35665f46aa1ecd
Pau Perez <saulopefa@gmail.com>
2022-09-10 18:33:09 +0200

    Tear app apart and start from scratch
    I will rewrite the features one by one.

--- App.js
+++ App.js
@@ -1,66 +1,20 @@
-import React from 'react';
-import { WebView, BackHandler } from 'react-native';
-import { Constants, Notifications } from 'expo';
-import injectCustomJavaScript from './lib/injectCustomJavaScript';
-import handleExternalLinks from './lib/handleExternalLinks';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-const baseUrl = () => {
-  const { releaseChannel } = Expo.Constants.manifest;
-
-  return (releaseChannel === 'staging') ?
-    'https://staging.timeoverflow.org' :
-    'https://www.timeoverflow.org';
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { currentUrl: baseUrl() };
-  }
-
-  componentDidMount() {
-    this._notificationSubscription = Notifications.addListener(this.onReceiveNotification);
-    BackHandler.addEventListener('hardwareBackPress', this.onBackPress);
-  }
-
-  componentWillUnmount() {
-    BackHandler.removeEventListener('hardwareBackPress', this.onBackPress);
-  }
-
-  // Check the notification attributes:
-  // https://docs.expo.io/versions/latest/guides/push-notifications#notification-handling-timing
-  onReceiveNotification = (notification = {}) => {
-    if (notification.origin === 'selected') {
-      const { data } = notification;
-
-      if (!data) return;
-      if (!data.url) return;
-
-      this.setState({ currentUrl: `${baseUrl()}${data.url}` });
-    }
-  }
-
-  onNavigationStateChange = ({ url }) => {
-    injectCustomJavaScript(this.webview, url);
-    handleExternalLinks(this.webview, url)
-  }
-
-  onBackPress = () => {
-    this.webview.goBack();
-
-    return true;
-  }
-
-  render() {
-    return (
-      <WebView
-        ref={ref => (this.webview = ref)}
-        source={{ uri: this.state.currentUrl }}
-        style={{marginTop: Constants.statusBarHeight}}
-        onNavigationStateChange={this.onNavigationStateChange}
-        scalesPageToFit={false}
-      />
-    );
-  }
-}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
